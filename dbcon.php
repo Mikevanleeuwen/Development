1b0cabd8c1acb0e53bd6ceca01ae792b806c563d
Mikevanleeuwen <mikedigid@hotmail.nl>
2016-02-12 16:08:50 +0100

    einde les 12-2

---
+++ dbcon.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$msqli = mysqli_connect('127.0.0.1', 'root', 'usbw', 'main');
+if(mysqli_connect_errno($msqli)){
+    echo 'failed to connect to MSQL: '.mysqli_connect_error();
+}
+
+
+$sql = "SELECT * FROM products";
+$res = mysqli_query($msqli,$sql);
+//var_dump($res);
+$row = mysqli_fetch_all($res);
+//var_dump($row);
+$msqli -> close();
+foreach($row as $value){
+    foreach($value as $element){
+        echo $element." ";
+    }
+    echo'<br>';
+}
