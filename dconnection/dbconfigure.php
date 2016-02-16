c7f9b8c89e1f7d715315c8a3fc7738e571b2e2bc
Mikevanleeuwen <mikedigid@hotmail.nl>
2016-02-16 10:39:46 +0100

    db handler

---
+++ dconnection/dbconfigure.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 16-2-2016
+ * Time: 09:01
+ */
+class dbconfigure
+{
+    public $host = "127.0.0.1";
+    public $user = "root";
+    public $passw = "usbw";
+    public $name = "main";
+
+
+
+
+}
