ab9fb02bbb8b31a46f5ed37ca54fc84802bec361
transliterate <mark_avontuur@hotmail.com>
2016-02-16 11:13:19 +0100

    did includes

--- loginssystem/register.php
+++ loginssystem/register.php
@@ -1,45 +1,6 @@
 <?php
 
-// Turn off all error reporting
-error_reporting(0);
-
-// Report simple running errors
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
-// Reporting E_NOTICE can be good too (to report uninitialized
-// variables or catch variable name misspellings ...)
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
-// Report all errors except E_NOTICE
-error_reporting(E_ALL & ~E_NOTICE);
-
-// Report all PHP errors (see changelog)
-error_reporting(E_ALL);
-
-// Report all PHP errors
-error_reporting(-1);
-
-// Same as error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-
-foreach ($_POST as $k => $v)	$$k = $v;
-
-$servername = "localhost";
-$username = "root@localhost";
-$password = "";
-$dbname = "test";
-$usremail = "";
-$usr = "";
-$usrpw = "";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-	echo "connection failed";
-} 
-	echo "connection succesfully";
+include 'dbconnection.php';
 
 $usremail = $_POST['email'];
 $usr = $_POST['usrnam'];
