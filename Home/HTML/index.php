2a356f517b36b45f58dda4f9f1763bbc9765d57b
transliterate <mark_avontuur@hotmail.com>
2016-02-26 12:47:55 +0100

    added index.php

---
+++ Home/HTML/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+			<link href="../../Bootstrap/css/bootstrap.css" rel="stylesheet">
+			<link rel="stylesheet" type="text/css" href="../CSS/index.css">
+		</head>
+		<body>
+			<div class="around">
+				<div class="container">
+					<div class="row logotxt">
+						<div class="logoplace">
+							<img src='../images/nslogo.png' style="width: 350px; height: 100px;">
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid arrows">
+					<div class="row">
+						<div class="pull-right">
+							<ul class="glyphicon glyphicon-chevron-right"></ul>
+						</div>
+						<div class="pull-left">
+							<ul class="glyphicon glyphicon-chevron-left"></ul>
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid">
+					<div class="row-fluid">
+						<div class="headertxt">
+							<p><b>Gemeente Bronto Bay</b></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
